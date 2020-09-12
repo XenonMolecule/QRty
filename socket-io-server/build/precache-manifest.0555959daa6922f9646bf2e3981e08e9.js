d009cfa227b32bd6124cc721420899ff0199922f
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-09-12 17:32:25 -0400

    Added build file to socket-io-server

---
+++ socket-io-server/build/precache-manifest.0555959daa6922f9646bf2e3981e08e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108859b0f05668c8065f1d7df7d86000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54973e4949d763d43dba",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8d4c64e07af262384354",
+    "url": "/static/css/main.ad84f4ef.chunk.css"
+  },
+  {
+    "revision": "54973e4949d763d43dba",
+    "url": "/static/js/2.ed94dbbd.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.ed94dbbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4c64e07af262384354",
+    "url": "/static/js/main.b2a2dd35.chunk.js"
+  },
+  {
+    "revision": "42a746f39177499e55df",
+    "url": "/static/js/runtime-main.7914c254.js"
+  }
+]);
