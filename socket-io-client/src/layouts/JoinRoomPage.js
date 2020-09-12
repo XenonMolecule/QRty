59848eeceb9d418a88acb57aa3355296b924fe93
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-09-12 04:14:11 -0400

    Made join room look :fire:

--- socket-io-client/src/layouts/JoinRoomPage.js
+++ socket-io-client/src/layouts/JoinRoomPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import socketIOClient from "socket.io-client";
 
 import Container from "react-bootstrap/Container";
@@ -6,22 +6,47 @@ import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
-
-const ENDPOINT = "http://127.0.0.1:4001";
+import Button from "react-bootstrap/Button";
 
 export default function MessengerPage() {
-    const [socket, setSocket] = useState(socketIOClient(ENDPOINT));
+    const txt0 = useRef(null);
+    const txt1 = useRef(null);
+    const txt2 = useRef(null);
+    const txt3 = useRef(null);
+    const txt4 = useRef(null);
+    const txt5 = useRef(null);
+    let refs = [txt0, txt1, txt2, txt3, txt4, txt5];
 
     useEffect(() => {
-        // CLEAN UP THE EFFECT
-        return () => socket.disconnect();
+        txt0.current.focus();
     }, []);
 
     let roomCode = [];
 
     for (let i = 0; i < 6; i ++) {
         roomCode.push(<Form.Group as={Col} controlId={"roomID-" + i}>
-            <Form.Control />
+            <Form.Control ref={refs[i]} style={{'height':'150px','fontSize':'60px','textAlign':'center'}} onChange = {(e) => {
+                if (e.target.value != "") {
+                    if (e.target.value.length > 1) {
+                        e.target.value = e.target.value[e.target.value.length - 1];
+                    }
+                    if(i < 5) {
+                        refs[i + 1].current.focus();
+                    }
+                } else {
+                    if (i > 0) {
+                        refs[i - 1].current.focus();
+                    }
+                }
+            }}
+            onKeyDown = {(e) => {
+                if (e.keyCode == 8) {
+                    if (i > 0){
+                        e.target.value = "";
+                        refs[i-1].current.focus();
+                    }
+                }
+            }}/>
         </Form.Group>);
     }
 
@@ -36,10 +61,21 @@ export default function MessengerPage() {
             <Row className="justify-content-center">
                 <p>Enter the 6-character room code below:</p>
             </Row>
-            <Row className="justify-contentcenter">
+            <Row className="justify-content-center">
                 <Form>
                     <Form.Row>
                         {roomCode}
+                    </Form.Row>
+                    <Form.Row className={"justify-content-center"}>
+                        <Button size={"lg"} style={{"height":"50px"}} block onClick={()=> {
+                            let room = "";
+                            for(let i = 0; i < 6; i ++) {
+                                room += refs[i].current.value;
+                            }
+                            if (room.length == 6) {
+                                window.location = "/" + room;
+                            }
+                        }}>Join</Button>
                     </Form.Row>
                 </Form>
             </Row>
