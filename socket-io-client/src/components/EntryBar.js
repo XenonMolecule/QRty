07194fa83fcc83b1fec083801df542f1aef34be3
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-09-11 22:14:41 -0400

    Added textbar and send button

--- socket-io-client/src/components/EntryBar.js
+++ socket-io-client/src/components/EntryBar.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 export default function EntryBar() {
 
     return (
-        <p>Text box</p>
+        <InputGroup>
+            <FormControl as="textarea" aria-label="With textarea" style={{'resize':'none'}} />
+            <InputGroup.Append>
+                <Button variant="info" style={{'width':'100px'}}>Send</Button>
+            </InputGroup.Append>
+        </InputGroup>
     );
 }
--- socket-io-client/src/layouts/MessengerPage.js
+++ socket-io-client/src/layouts/MessengerPage.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
+
 import EntryBar from "../components/EntryBar";
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar";
+
 const ENDPOINT = "http://127.0.0.1:4001";
 
 export default function MessengerPage() {
@@ -16,9 +20,13 @@ export default function MessengerPage() {
     }, []);
 
     return (
-        <>
+        <Container>
             <h1>This is the messenger page</h1>
-            <EntryBar></EntryBar>
-        </>
+            <Navbar bg="light" expand="lg" fixed={"bottom"}>
+                <Container>
+                    <EntryBar></EntryBar>
+                </Container>
+            </Navbar>
+        </Container>
     );
 }
