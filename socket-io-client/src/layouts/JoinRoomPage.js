ebb296cf2efe472181b8b978102d8c11c1c099cf
David Cornell <davidcornell2326@gmail.com>
2020-09-12 03:26:58 -0400

    Better spacing on screen sizes

--- socket-io-client/src/layouts/JoinRoomPage.js
+++ socket-io-client/src/layouts/JoinRoomPage.js
@@ -23,13 +23,13 @@ export default function MessengerPage() {
             <Navbar bg="light">
                 <Navbar.Brand href="/">Return Home</Navbar.Brand>
             </Navbar>
-            <Row className="justify-content-md-center" style={{'text-transform': 'uppercase'}}>
+            <Row className="justify-content-center" style={{'text-transform': 'uppercase'}}>
                 <h1>Join a Room</h1>
             </Row>
-            <Row className="justify-content-md-center">
+            <Row className="justify-content-center">
                 <p>Enter the 6-character room code below:</p>
             </Row>
-            <Row className="justify-content-md-center">
+            <Row className="justify-content-center">
                 <EntryBar btnText={"Join"} send={(text) => {
                         window.location=text;
                     }}/>
